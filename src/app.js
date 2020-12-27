33efdbda330d6903c30d77b72b2b5f715bbda2ca
achyuth reddy <ncsachyuth@gmail.com>
2020-12-27 15:27:33 +0530

    push

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({ msg: 'Hello' })
+  res.json({ msg: 'Hello', status: 201 })
 })
 app.get('/hello', (req, res) => {
-  res.json({ msg: 'Hello' })
+  res.json({ msg: 'Hello', status: 200 })
 })
 app.listen(5000, () => {
   console.log('running ar port 5000')
